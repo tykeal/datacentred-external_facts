a12def01550ea0a87dbbfde1c385196769559ced
Andrew Grimberg <tykeal@bardicgrove.org>
2021-04-23 16:47:45 -0700

    Refactor: Rubocop linting
    Fix the rubocop linting errors before fully converting to PDK
    
    Signed-off-by: Andrew Grimberg <tykeal@bardicgrove.org>

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,71 +1,83 @@
 require 'spec_helper'
 describe 'external_facts' do
   context 'FOSS < 4.0' do
-    let(:facts) {
+    let(:facts) do
       {
-        :is_pe         => false,
-        :puppetversion => '2.7.1'
+        is_pe: false,
+        puppetversion: '2.7.1'
       }
+    end
+
+    it { is_expected.to contain_class('external_facts') }
+    it { is_expected.to contain_class('external_facts::params') }
+
+    it {
+      is_expected.to contain_file('facter basedir').with(
+        'path' => '/etc/facter',
+      )
     }
-
-    it { should contain_class('external_facts') }
-    it { should contain_class('external_facts::params') }
-
-    it { should contain_file('facter basedir').with(
-      'path' => '/etc/facter',
-    ) }
-    it { should contain_file('facts dir').with(
-      'path'    => '/etc/facter/facts.d',
-      'recurse' => true,
-      'purge'   => true,
-    ) }
+    it {
+      is_expected.to contain_file('facts dir').with(
+        'path'    => '/etc/facter/facts.d',
+        'recurse' => true,
+        'purge'   => true,
+      )
+    }
   end
 
   context 'FOSS >= 4.0' do
-    let(:settings) {
+    let(:settings) do
       {
-        :environmentpath => '/etc/puppetlabs'
+        environmentpath: '/etc/puppetlabs'
       }
-    }
-    let(:facts) {
+    end
+    let(:facts) do
       {
-        :is_pe         => false,
-        :puppetversion => '4.2.1',
+        is_pe: false,
+        puppetversion: '4.2.1',
       }
-    }
+    end
 
-    it { should contain_file('facter basedir').with(
-      'path' => '/etc/puppetlabs/facter',
-    ) }
-    it { should contain_file('facts dir').with(
-      'path'    => '/etc/puppetlabs/facter/facts.d',
-      'recurse' => true,
-      'purge'   => true,
-    ) }
+    it {
+      is_expected.to contain_file('facter basedir').with(
+        'path' => '/etc/puppetlabs/facter',
+      )
+    }
+    it {
+      is_expected.to contain_file('facts dir').with(
+        'path'    => '/etc/puppetlabs/facter/facts.d',
+        'recurse' => true,
+        'purge'   => true,
+      )
+    }
   end
 
   context 'PE' do
-    let(:settings) {
+    let(:settings) do
       {
-        :environmentpath => '/etc/puppetlabs'
+        environmentpath: '/etc/puppetlabs'
       }
-    }
-    let(:facts) {
+    end
+    let(:facts) do
       {
-        :is_pe         => true,
-        :pe_version    => '3.8.1',
-        :puppetversion => '3.8.1 (Puppet Enterprise 3.8.1)',
+        is_pe: true,
+        pe_version: '3.8.1',
+        puppetversion: '3.8.1 (Puppet Enterprise 3.8.1)',
       }
-    }
+    end
 
-    it { should contain_file('facter basedir').with(
-      'path' => '/etc/puppetlabs/facter',
-    ) }
-    it { should contain_file('facts dir').with(
-      'path'    => '/etc/puppetlabs/facter/facts.d',
-      'recurse' => true,
-      'purge'   => true,
-    ) }
+    it {
+      is_expected.to contain_file('facter basedir').with(
+        'path' => '/etc/puppetlabs/facter',
+      )
+    }
+    it {
+      is_expected.to contain_file('facts dir').with(
+        'path'    => '/etc/puppetlabs/facter/facts.d',
+        'recurse' => true,
+        'purge'   => true,
+      )
+    }
   end
 end
 
--- spec/defines/fact_spec.rb
+++ spec/defines/fact_spec.rb
@@ -1,30 +1,33 @@
 require 'spec_helper'
-describe 'external_facts::fact', :type => :define do
+describe 'external_facts::fact' do
   # We've tested that the facts.d gets set correctly in the init_spec so
   # we'll only test the resultant path on a FOSS Puppet < 4 configuration
-  let(:facts) {
+  let(:facts) do
     {
-      :is_pe         => false,
-      :puppetversion => '2.7.1',
+      is_pe: false,
+      puppetversion: '2.7.1',
     }
-  }
+  end
   let(:title) { 'testing' }
 
   context 'with defaults for all parameters' do
-    let (:params) {{}}
+    let(:params) do
+      {}
+    end
 
-    it { should contain_class('external_facts') }
+    it { is_expected.to contain_class('external_facts') }
 
-    it { should contain_file('fact testing').with(
-      'ensure'  => 'file',
-      'path'    => '/etc/facter/facts.d/testing.txt',
-      'owner'   => 'root',
-      'group'   => 'root',
-      'mode'    => '0644',
-      'content' => 'testing=true',
-    ) }
+    it {
+      is_expected.to contain_file('fact testing').with(
+        'ensure'  => 'file',
+        'path'    => '/etc/facter/facts.d/testing.txt',
+        'owner'   => 'root',
+        'group'   => 'root',
+        'mode'    => '0644',
+        'content' => 'testing=true',
+      )
+    }
   end
 end
 
 # vim: ts=2 sw=2 sts=2 et :
-
